0dc25858985ad98f8bdf8c17b7d5783aec72c207
chu.reybr <chu.reybr@gmail.com>
2023-10-27 00:12:09 +0800

    Created dynamic text for Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+const text = ['Crucial', 'Core', 'Important', 'Fundamental'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {text[genRandomInt(3)]} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
