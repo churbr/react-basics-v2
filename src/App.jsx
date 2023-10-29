7c7e1bae4a95d77f26a3774b48376b930b6497f9
chu.reybr <chu.reybr@gmail.com>
2023-10-30 02:19:42 +0800

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
-import reactImage from './assets/react-core-concepts.png'
+import reactImage from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import propsImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
 
-const text = ['Crucial', 'Core', 'Important', 'Fundamental'];
+const CORE_CONCEPTS = [
+  {
+    image: componentsImg,
+    title: "Components",
+    description:
+      "The core UI building block - compose the user interface by combining multiple components.",
+  },
+  {
+    image: jsxImg,
+    title: "JSX",
+    description:
+      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+  },
+  {
+    image: propsImg,
+    title: "Props",
+    description:
+      "Make components configurable (and therefore reusable) by passing input data to them.",
+  },
+  {
+    image: stateImg,
+    title: "State",
+    description:
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+  },
+];
+
+const text = ["Crucial", "Core", "Important", "Fundamental"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -12,9 +43,21 @@ function Header() {
       <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {text[genRandomInt(3)]} React concepts you will need for almost any app you are going to build!
+        {text[genRandomInt(3)]} React concepts you will need for almost any app
+        you are going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept({ title, image, description }) {
+  // Destructure props
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
@@ -23,7 +66,19 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concept!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
